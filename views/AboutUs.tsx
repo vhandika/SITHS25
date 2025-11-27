99e119b6921d72c0d1ee5c0ab1afeff8274ef7af
vhandika <vhandikanizarj@gmail.com>
2025-11-27 18:00:42 +0700

    Update AboutUs.tsx

--- views/AboutUs.tsx
+++ views/AboutUs.tsx
@@ -3,11 +3,11 @@ import { Users } from 'lucide-react';
 
 const About: React.FC = () => {
     return (
-        <div className="min-h-screen w-full bg-black py-16 lg:py-24 px-4 sm:px-6 lg:px-8 mt-16 lg:mt-0 font-sans">
+        <div className="min-h-screen w-full bg-black py-16 lg:py-24 px-4 sm:px-6 lg:px-8 mt-16 lg:mt-0 font-sans overflow-y-scroll">     
             <div className="mx-auto max-w-7xl text-center">
                 <div className="text-center">
                     <div className="flex justify-center items-center gap-4 mb-4">
-                         <div className="w-10 h-10 flex items-center justify-center bg-yellow-400 text-black transform -skew-x-12">
+                        <div className="w-10 h-10 flex items-center justify-center bg-yellow-400 text-black transform -skew-x-12">
                             <span className="transform skew-x-12"><Users size={32} /></span>
                         </div>
                         <h1 className="text-4xl font-bold tracking-wider uppercase text-white sm:text-5xl">About Us</h1>
@@ -15,9 +15,9 @@ const About: React.FC = () => {
                 </div>
 
                 <div className="w-40 h-1 bg-yellow-400 mx-auto mt-8"></div>
+
                 <div className="mx-auto mt-8 max-w-4xl space-y-8 text-gray-400 text-center">
                     <p>
-
                     </p>
                 </div>
             </div>
