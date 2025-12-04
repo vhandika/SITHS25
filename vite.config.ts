2ddd2dd22041b720767c0431f5ee540822fa0b98
vhandika <vhandikanizarj@gmail.com>
2025-12-04 07:45:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,47 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import obfuscator from 'rollup-plugin-javascript-obfuscator';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    
+    const isProduction = mode === 'production';
+
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+      plugins: [
+        react(),
+        isProduction && obfuscator({
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 1,
+            deadCodeInjection: true,
+            debugProtection: true,
+            disableConsoleOutput: true,
+            identifierNamesGenerator: 'hexadecimal',
+            log: false,
+            numbersToExpressions: true,
+            renameGlobals: false,
+            rotateStringArray: true,
+            selfDefending: true,
+            stringArray: true,
+            stringArrayEncoding: ['rc4'],
+            splitStrings: true,
+            simplify: true,
+            include: ['**/*.js', '**/*.ts', '**/*.tsx'],
+            exclude: ['node_modules/**']
+        })
+      ],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        sourcemap: false,
       }
     };
 });
