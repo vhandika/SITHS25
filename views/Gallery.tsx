a520f131165a95456b92d0f962f4fedad254dc0d
AndhikaNizar <andhikazanuar81@google.com>
2026-03-17 12:50:04 +0700

    speed fix

--- views/Gallery.tsx
+++ views/Gallery.tsx
@@ -36,7 +36,7 @@ const ScrollingTitle: React.FC<{ title: string }> = ({ title }) => {
                 if (textWidth > parentWidth) {
                     setIsOverflowing(true);
                     
-                    const speed = 15; 
+                    const speed = 30; 
                     setDuration((textWidth + 32) / speed); 
                 } else {
                     setIsOverflowing(false);
